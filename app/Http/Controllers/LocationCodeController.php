9f2073b25ebf079d82cc871778fd18f98fc53624
Illiyan Ainun Qalbi <nullroku@gmail.com>
2017-05-25 20:28:27 +0800

    LocationCode v.0.0

---
+++ app/Http/Controllers/LocationCodeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\LocationCode;
+use Illuminate\Http\Request;
+
+class LocationCodeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\LocationCode  $locationCode
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LocationCode $locationCode)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\LocationCode  $locationCode
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LocationCode $locationCode)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\LocationCode  $locationCode
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LocationCode $locationCode)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\LocationCode  $locationCode
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LocationCode $locationCode)
+    {
+        //
+    }
+}
